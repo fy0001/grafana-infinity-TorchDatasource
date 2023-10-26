3592f5f13e7c5f2d6495c7bbe49751fae6ce4377
Faith Young <fyoung@torchtechnologies.com>
2023-10-26 11:29:20 -0500

    Update.

--- webpack.config.ts
+++ webpack.config.ts
@@ -16,9 +16,14 @@ const config = async (env): Promise<Configuration> => {
     resolve: {
       fallback: {
         crypto: require.resolve('crypto-browserify'),
-        fs: false,
-        path: require.resolve('path-browserify'),
+        url: require.resolve('url'),
+        fs: require.resolve('fs'),
+        assert: require.resolve('assert'),     
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'), 
         stream: require.resolve('stream-browserify'),
+        path: require.resolve('path-browserify'),
         util: require.resolve('util'),
         timers: require.resolve("timers-browserify"),
         buffer: require.resolve("buffer")
